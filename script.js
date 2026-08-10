c0e470f327837571015ec2653d249a6a1faba5fa
fofanaumar73-ai <fofanaumar73@gmail.com>
2026-08-10 04:06:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Elite Gadgets is ready!");
+const productButtons = document.querySelectorAll(".product-btn");
+
+productButtons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        alert("Product selected!");
+    });
+});
